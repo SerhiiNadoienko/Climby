bc5bdb319eec45ba728c4306eb9959bf9670ee1c
s.nadoienko <s.nadoienko@andersenlab.com>
2024-08-30 17:05:06 +0200

    added general styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/styles/index.css';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './pages/App';
+import { store } from 'store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
